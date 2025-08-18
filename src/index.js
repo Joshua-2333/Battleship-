26bee9e19340b125de0cba014cfada89ea7fde19
Joshua-2333 <jaflores.2333@gmail.com>
2025-08-17 20:54:30 -0500

    Finished the intro screen/added new images

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import './styles.css';
+
 // Import images
 import logoImg from './images/logo.png';
 import scrollBgImg from './images/pirateScroll.png';
 import startBtnImg from './images/start_btn.png';
 import player1Img from './images/player1.png';
 import player2Img from './images/player2.png';
+import woodTextureImg from './images/wood_texture.jpg';
 
 // Import audio
 import battleSong from './sfx/battleSong.mp3';
 import hitSound from './sfx/hitSound.mp3';
 import missSound from './sfx/missSound.mp3';
+
+// Set wood background for body
+document.body.style.backgroundImage = `url(${woodTextureImg})`;
+document.body.style.backgroundSize = 'cover';
+document.body.style.backgroundRepeat = 'no-repeat';
 
 // Attach images
 document.getElementById('logo').src = logoImg;
@@ -22,12 +29,13 @@ const startBtn = document.getElementById('start-btn');
 startBtn.style.backgroundImage = `url(${startBtnImg})`;
 startBtn.style.backgroundSize = 'contain';
 startBtn.style.backgroundRepeat = 'no-repeat';
-startBtn.style.width = '120px';
-startBtn.style.height = '50px';
-startBtn.setAttribute('aria-label', 'Start Game'); // accessibility
+startBtn.style.width = '250px';
+startBtn.style.height = '120px';
+startBtn.setAttribute('aria-label', 'Start Game'); 
 startBtn.style.cursor = 'pointer';
 
 // Attach audio
+const battleMusic = document.getElementById('battle-music');
 document.getElementById('battle-music').src = battleSong;
 document.getElementById('hit-sound').src = hitSound;
 document.getElementById('miss-sound').src = missSound;
@@ -35,8 +43,8 @@ document.getElementById('miss-sound').src = missSound;
 // Get DOM elements
 const introScreen = document.getElementById('intro-screen');
 const gameScreen = document.getElementById('game-screen');
-const battleMusic = document.getElementById('battle-music');
 const playerNameInput = document.getElementById('player-name');
+const logo = document.getElementById('logo');
 
 // Start Game button click handler
 startBtn.addEventListener('click', () => {
@@ -54,4 +62,18 @@ startBtn.addEventListener('click', () => {
   battleMusic.play().catch(err => console.log('Autoplay blocked:', err));
 
   console.log(`Captain ${playerName} has entered the battle!`);
+});
+
+// Logo click handler to return to intro screen
+logo.addEventListener('click', () => {
+  // Hide game, show intro
+  gameScreen.classList.add('hidden');
+  introScreen.classList.remove('hidden');
+
+  // Stop and reset music
+  battleMusic.pause();
+  battleMusic.currentTime = 0;
+
+  // Clear player name input
+  playerNameInput.value = '';
 });
